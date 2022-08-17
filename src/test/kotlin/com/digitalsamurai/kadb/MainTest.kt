6b2183580aa3598d031e966542f0d49b29697b37
Andrey <nugaev.andrei@gmail.com>
2022-08-17 15:44:59 +0300

    update gradleto 7.5

--- src/test/kotlin/com/digitalsamurai/kadb/MainTest.kt
+++ src/test/kotlin/com/digitalsamurai/kadb/MainTest.kt
@@ -22,38 +22,9 @@ class MainTest  {
 
         println(test.startAdbServer())
         val answer = test.getDevices()
-//        CoroutineScope(Dispatchers.IO).launch {
-//            println(answer.clearAnswer)
-//            val q = test.sendCommand("logcat")
-//            println(q.data)
-//        }
-        val a = test.installApkOnDevice(answer.data[0], "D:\\.WORK_PROJECTS\\.partner\\PartnerApp2\\app\\build\\outputs\\apk\\debug\\a.apk")
-        println(a.requestStatus.toString()+" "+a.data.toString())
-        val b =test.Shell.ActivityManager.startApplicationOnDevice(answer.data[0],"com.apponewin.partnerapp/com.apponewin.partnerapp.MainActivity")
-        println(b.clearAnswer)
-        delay(10*1000)
 
-        val dump = test.Shell.UiAutomator.dump(answer.data[0])
+        println(answer)
 
-        if(dump.data!=null){
-            val pullAnswer = test.pullFileFromDevice("3fb71e7a",dump.data!!, "D:\\work")
-            println(pullAnswer.clearAnswer)
-            println("${pullAnswer.data?.first} : ${pullAnswer.data?.second}")
-
-            val file = File("D:\\work\\window_dump.xml")
-
-            println("FILE HASHCODE: ${file.readLines().hashCode()}")
-            //1362201386
-            //78308962 WEBVIEW
-            //78308962 WEBVIEW
-            //-1931483432 LOADING
-            //-1931483432 LOADING
-        }
-
-
-
-        delay(5000)
-        test.uninstallApkOnDevice(answer.data[0],"com.apponewin.partnerapp")
 
 
 
